ec335c5ff15372350b46486705fb963c4601589d
Jörgen Nilsson <me@jorgennilsson.com>
2019-03-15 13:33:23 +0100

    Adding third party graph and footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Geek Beer - Ett tech meetup`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -25,6 +25,17 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "Contentful",
+        // This is the field under which it's accessible
+        fieldName: "contentful",
+        // URL to query from
+        url: "http://localhost:4000",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/components/footer.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Footer = () => (
+  <footer
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <p>
+        Copyright ©© {new Date().getFullYear()} Geek Beer. All rights reserved.
+      </p>
+    </div>
+  </footer>
+)
+
+export default Footer
