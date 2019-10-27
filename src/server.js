cd4ed28b86de524798c25ab531b586b023c95336
Jorgen Nilsson <me@jorgennilsson.com>
2019-10-27 12:28:27 +0100

    Change in conf for how to use graphql server env

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
-import dotenv from 'dotenv'
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-dotenv.config();
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
