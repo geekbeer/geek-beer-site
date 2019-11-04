c8a021904e29b8a284d88a8d280a8a318619955f
Anthon Holmqvist <anthon.kendel@gmail.com>
2019-11-04 21:36:55 +0100

    Refactor meetups page
    Update the meetups page with destructuring and
    consistent code style.

--- src/routes/meetups/index.svelte
+++ src/routes/meetups/index.svelte
@@ -1,26 +1,25 @@
 <script context="module">
   import md from '../../markdown';
-  import {client} from '../../graphql/client';
+  import { client } from '../../graphql/client';
   import pageQuery from '../../graphql/queries/page.gql.js';
   import eventsQuery from '../../graphql/queries/events.gql.js';
 
   export async function preload() {
     const page = client.query({
       query: pageQuery,
-      variables: {"uri": "/meetups"}
+      variables: { uri: '/meetups' }
     });
 
     const events = client.query({
       query: eventsQuery
-
     });
 
-    const result = await Promise.all([page, events]);
+    const [pageResult, eventsResult] = await Promise.all([page, events]);
 
     return {
-      page: result[0].data.page,
-      events: result[1].data.events,
-    }
+      page: pageResult.data.page,
+      events: eventsResult.data.events
+    };
   }
 </script>
 
@@ -29,17 +28,16 @@
 </script>
 
 <svelte:head>
-	<title>{page.pageTitle}</title>
+  <title>{page.pageTitle}</title>
 </svelte:head>
 
 <article>
-{@html md(page.content[0].body)}
-<ul class="event-list">
+  {@html md(page.content[0].body)}
+  <ul class="event-list">
     {#each events as event}
-        <li>
-            <a href="/meetups/{event.uri}">{event.title}</a>
-        </li>
+      <li>
+        <a href="/meetups/{event.uri}">{event.title}</a>
+      </li>
     {/each}
-</ul>
+  </ul>
 </article>
-
