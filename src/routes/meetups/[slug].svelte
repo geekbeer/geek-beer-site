c5fdc0d03014885f86c9fed491cbbe46d3efc4d5
Anthon Holmqvist <anthon.kendel@gmail.com>
2019-11-04 21:36:42 +0100

    Fix "null" page on /meetups/[slug] page
    Some meetup pages displayed "null" because of the
    excerpt field not being handled.
    
    In this commit this is fixed by using a getOrElse()
    function that will return a fallback value (an empty string
    be default) if the value passed is not defined.
    
    In addition to this the description is added to the view as a
    paragraph.

--- src/routes/meetups/[slug].svelte
+++ src/routes/meetups/[slug].svelte
@@ -1,65 +1,30 @@
 <script context="module">
-  import {client} from '../../graphql/client';
+  import { client } from '../../graphql/client';
   import eventQuery from '../../graphql/queries/event.gql.js';
 
   export async function preload({ params }) {
     const result = await client.query({
       query: eventQuery,
-      variables: {"uri": params.slug}
+      variables: { uri: params.slug }
     });
 
     return {
-      event: result.data.event,
-    }
+      event: result.data.event
+    };
   }
 </script>
 
 <script>
-	export let event;
+  import { getOrElse } from '../../utils.js'; 
+  export let event;
 </script>
 
-<style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
-
-	.content :global(pre) {
-		background-color: #f9f9f9;
-		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
-
-	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
-	}
-
-	.content :global(ul) {
-		line-height: 1.5;
-	}
-
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
-	}
-</style>
-
 <svelte:head>
-	<title>{event.title}</title>
+  <title>{event.title}</title>
 </svelte:head>
 
-<h1>{event.title}</h1>
+<h2>{getOrElse(event.title)}</h2>
 
-<p>
-{event.excerpt}
-</p>
+<h3>{getOrElse(event.excerpt)}</h3>
+
+<p>{getOrElse(event.description)}</p>
